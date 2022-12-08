7efa0b5ef87734259160af513865aad9303b3e65
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2022-12-08 09:10:42 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * C program to print using puts
- * And should return 0
+ *main - C program to print using puts
+ * And should return: 0
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
