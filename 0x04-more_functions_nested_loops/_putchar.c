c3bbea3cb1d380f69d9af2e9d92a997d7f8f8cda
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2022-12-16 05:07:15 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,27 @@
+nclude "main.h"
+#include <unstd.h>
+/**
+ *  *_putchar- writes a character to stdout
+ *   *@c: is character to print
+ *    * Return:Success 1
+ *     * main-prints _putchar
+ *      *
+ *       * Return: always 0
+ *        *
+ *        */
+int _putchar(char c)
+	int main(void)
+{
+		return (write(1, &c, 1));
+			_putchar('_');
+				_putchar('p');
+					_putchar('u');
+						_putchar('t');
+							_putchar('c');
+								_putchar('h');
+									_putchar('a');
+										_putchar('r');
+											_putchar('\n');
+												return (0);
+}
+
