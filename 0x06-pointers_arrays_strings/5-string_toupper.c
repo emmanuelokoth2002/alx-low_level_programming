abfaada3193a41b45ee9236d95251b3ffac7f711
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2022-12-22 04:20:13 -0800

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_toupper-changes all lowercase letters
+ *  to upper case
+ * @str:parameter
+ * Returns-returns a character
+*/
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+	}
+	return (str);
+}
