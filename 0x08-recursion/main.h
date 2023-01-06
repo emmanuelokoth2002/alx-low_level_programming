1b8a7ea28ce948ed472c87d3572710a0b5d5b959
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-01-06 00:00:47 -0800

    Task 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square(int n, int val);
+int actual_prime(int n, int i);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
