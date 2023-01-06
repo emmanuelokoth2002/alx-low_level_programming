25f893b2f005f4964f03706fcce33377c0e69a09
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-01-05 20:40:19 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion- search for a string for any set of bytes
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the bytes
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 1)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
