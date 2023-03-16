4cb1818032c8536de79215f15d2ee3b293f27c28
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-03-16 13:35:48 -0700

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,11 @@
-ize_t print_dlistint(const dlistint_t *h)
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes in list
+*/
+size_t print_dlistint(const dlistint_t *h)
 {
 	 size_t count = 0;
 	 const dlistint_t *current = h;
