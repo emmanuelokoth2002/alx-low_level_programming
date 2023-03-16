848fd95d4a66d22c5640157dd55ed692f15a677d
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-03-16 13:27:37 -0700

    Task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,13 @@
+ize_t print_dlistint(const dlistint_t *h)
+{
+	 size_t count = 0;
+	 const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		 printf("%d\n", current->n);
+		 current = current->next;
+		 count++;
+	}
+	return (count);
+}
