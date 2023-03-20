1174971e514f1dbc6c934068f7ce62b18dbc7b99
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-03-20 03:39:37 -0700

    Task 1

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy-C function that copies a string
+ * @dest:buffer storing a string copy
+ * @src:the source string
+ * @n:number of byte copy
+ * Return:returns
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
